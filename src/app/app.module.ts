802389bb70017d13fc233a5ec2a6df2f0e0bb798
Gerardo Rodarte <gerardo.rodarte.013@gmail.com>
2018-05-11 13:26:28 -0700

    se crearon los componentes tas,navbar,task-list,task-add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TaskAddComponent } from './components/task-add/task-add.component';
+import { TaksListComponent } from './components/taks-list/taks-list.component';
+import { TaksComponent } from './components/taks/taks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TaskAddComponent,
+    TaksListComponent,
+    TaksComponent
   ],
   imports: [
     BrowserModule
